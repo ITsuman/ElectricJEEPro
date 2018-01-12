b67a1b4047240b714515238deb6f6ada1d90c983
ITsuman <30381482+ITsuman@users.noreply.github.com>
2018-01-12 22:00:31 +0530

    Update GetDb.java

--- power1/src/module/GetDb.java
+++ power1/src/module/GetDb.java
@@ -2,10 +2,10 @@ package module;
 import java.sql.*;
 public class GetDb {
 	static Connection cn=null;
-	static String driver="com.ibm.db2.jcc.DB2Driver";
-	static String url="jdbc:db2://localhost:50000/Customer";
-	static String user="Destiny";
-	static String pass="techno6sk";
+	static String driver="com.ibm.db2.jcc.DB2Driver";//DB2 database used 
+	static String url="";//Database URL
+	static String user="";//Username
+	static String pass="";//Password
 	public static Connection GetConnection(){	
 		try{
 		Class.forName(driver);
